043d5145c093bd831cd0ce7bd6ae501ed9c9f559
dajitui <839737769@qq.com>
2018-08-05 17:23:13 +0800

    Update KafkaProperties.java

--- src/main/java/com/example/demo/KafkaProperties.java
+++ src/main/java/com/example/demo/KafkaProperties.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 public interface KafkaProperties
 {
-    final static String zkConnect = "123.207.111.178:2181";
+    final static String zkConnect = "xxx:2181";
     final static String groupId = "group1";
     final static String topic = "hi";
-    final static String kafkaServerURL = "123.207.111.178";
+    final static String kafkaServerURL = "ip地址";
     final static int kafkaServerPort = 9092;
     final static int kafkaProducerBufferSize = 64 * 1024;
     final static int connectionTimeOut = 20000;
